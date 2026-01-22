b5378d2c9de3fca8b79c7372c24dbaa814e01f68
Sandeep Kumar <135053219+sandeepwebsite@users.noreply.github.com>
2026-01-22 11:55:36 +0530

    Implement service worker for caching assets

--- water-sort/sw.js
+++ water-sort/sw.js
@@ -1,1 +1,37 @@
+const CACHE_NAME = "water-sort-v1";
+const ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./images/water-sort.png",
+  "./images/reset.png",
+  "./images/undo.png",
+  "./images/level-up.png",
+  "./sound/pour.mp3",
+  "./sound/win.mp3"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
 
